6ffebafdcef919bf0ed9aadcda51817bf1c0c756
Daniel Tian <danieltian31@yahoo.com>
2019-06-04 19:37:20 -0500

    added github profile route

--- routes/api/profile/profile.js
+++ routes/api/profile/profile.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const router = express.Router();
+const request = require("request");
+const config = require("config");
 const auth = require("../../../middleware/auth")
 const {check, validationResult} = require("express-validator/check")
 
@@ -314,16 +316,55 @@ router.delete("/experience/:exp_id", auth, async (req, res)=>{
 
  router.delete("/education/:edu_id", auth, async (req, res)=>{
 
-    const profile = await Profile.findOne({user: req.user.id});
+    try {
+        const profile = await Profile.findOne({user: req.user.id});
 
-    const removeIndex = profile.education.map(edu => edu.id).indexOf(req.params.edu_id)
+        const removeIndex = profile.education.map(edu => edu.id).indexOf(req.params.edu_id)
+    
+        profile.education.splice(removeIndex, 1);
+    
+        await profile.save();
+    
+        res.json(profile);
+        
+    } catch (err) {
 
-    profile.education.splice(removeIndex, 1);
+        console.log(err.message);
+        res.status(500).send("Server Error");
+    }
 
-    await profile.save();
 
-    res.json(profile);
+ })
 
+  //@route  GET api/profile/github/:username
+ //@desc   Get user repos from Github
+ //@access Public
+
+ router.get("/github/:username", (req,res)=>{
+     try {
+        
+        const options = {
+            uri: `https://api.github.com/users/${req.params.username}/repos?per_page=5&sort=created:asc&client_id=${config.get("githubClientId")}&client_secret=${config.get("githubSecret")}`,
+            method: "GET",
+            headers: {"user-agent":"node.js"}
+        }
+
+        request(options, (err, response, body)=>{
+            if(err) console.log(err);
+
+            if(response.statusCode !== 200){
+                res.status(404).json({msg : "No Github profile found"});
+            }
+
+            res.json(JSON.parse(body));
+
+        })
+
+     } catch (err) {
+         
+        console.log(err.message);
+        res.status(500).send("Server Error");
+     }
  })
 
 module.exports = router;
